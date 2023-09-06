c0f4a31d48f21ce5f8788f86de21562682a98b16
Taka <takao.shimose01@gmail.com>
2023-09-07 00:40:41 +0900

    takao remote share

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,5 @@
+class ArticlesController < ApplicationController
+    def index
+        render :index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', to: 'articles#index', as: 'index_article'
 end
